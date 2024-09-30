2a241315f764dd4797127280c62878a02bb6ff10
w0r00d <worood.madwar@gmail.com>
2024-09-30 09:17:54 +0300

    minor changes

--- app/Filament/Resources/BeneficiaryResource.php
+++ app/Filament/Resources/BeneficiaryResource.php
@@ -18,7 +18,8 @@ class BeneficiaryResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
-    // protected static bool $shouldRegisterNavigation = false;
+    protected static bool $shouldRegisterNavigation = false;
+
     public static function form(Form $form): Form
     {
         return $form
