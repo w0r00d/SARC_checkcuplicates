56ec54f94e686df3fe7692ed88cd09b9b9c9b2c2
w0r00d <worood.madwar@gmail.com>
2024-10-02 15:24:46 +0300

    migration

--- database/migrations/2024_10_02_121833_create_pending_beneficiaries_table.php
+++ database/migrations/2024_10_02_121833_create_pending_beneficiaries_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('pending_beneficiaries', function (Blueprint $table) {
             $table->id();
+
             $table->timestamps();
         });
     }
